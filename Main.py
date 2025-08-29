a00bf166fef620a27486032871f8be0489796ecd
Mohanrpk <mohanrpk@gmail.com>
2025-08-29 21:17:03 +0530

    commit

--- Main.py
+++ Main.py
@@ -1,35 +1,60 @@
 class bank:
     def __init__(self, account_number, balance):
         self.account_number = account_number
-        self.balance = balance
+        self._balance = balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f"Deposited: {amount}. New balance: {self.balance}")
-        else:
-            print("Enter valid amount")
+        self._balance += amount
+
+    def get_balance(self):
+        return self._balance
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance")
-        else:
-            self.balance -= amount
-            print(f"Withdrawn: {amount}. New balance: {self.balance}")
+       
+        self._balance -= amount
+    def get_balance(self):
+        return self._balance
 
 account1 = bank(1234567890, 500)
 account1.deposit(200)
 account1.withdraw(800)
+print(f"Account Number: {account1.account_number}, Balance: {account1.get_balance()}")
 
-class savings_account(bank):
-    def __init__(self, account_number, balance, interest_rate):
-        super().__init__(account_number, balance)
-        self.interest_rate = interest_rate
 
-    def add_interest(self):
-        interest = self.balance * self.interest_rate / 100
-        self.balance += interest
-        print(f"Interest added: {interest}. New balance: {self.balance}")
-savings1 = savings_account(9876543210, 500, 5)
-savings1.add_interest()
+#Employee Management
+class RegularEmployee:
+    def __init__(self, name, salary, bonus):
+        self.name = name
+        self.salary = salary
+        self.bonus = bonus
+
+    def describe(self):
+        return(f"The employee {self.name} is a regular employee and has earned {self.salary + self.bonus}.")
+
+class ContractEmployee:
+    def __init__(self, name, rate_per_hour, hours_worked):
+        self.name = name
+        self.rate_per_hour = rate_per_hour
+        self.hours_worked = hours_worked
+    def describe(self):
+        return(f"The employee {self.name} is a contract employee and has earned {self.hours_worked * self.rate_per_hour}.")
+class Manager:
+    def __init__(self, name, salary, allowance):
+        self.name = name
+        self.salary = salary
+        self.allowance = allowance
+    def describe(self):
+        return(f"The employee {self.name} is a manager and has earned {self.salary +self.allowance}.")
+
+
+emp1 = RegularEmployee("Mohan", 50000, 5000)
+emp2 = ContractEmployee("Karthik", 100, 40)
+emp3 = Manager("Dhanwanth", 70000, 10000)
+
+print(emp1.describe())
+print(emp2.describe())
+print(emp3.describe())
+
+
+
 
